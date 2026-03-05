d131a8349320f0efa78a1d1f58cc875b7e89d7f3
Khalil Yusuf <khalil.yusuf@hiof.no>
2026-03-05 14:36:03 +0000

    Merged PR 15: feat: created IMultiSeriesIndicator, an Interface for indicators that operate...
    feat: created IMultiSeriesIndicator, an Interface for indicators that operate on 2+ series
    
    Related work items: #30

---
+++ LovelaceGroup6/src/StockAnalysis/Interfaces/IMultiSeriesIndicator.cs
@@ -0,0 +1,17 @@
+using LovelaceGroup6.StockAnalysis.Models;
+
+namespace LovelaceGroup6.StockAnalysis.Interfaces;
+
+/// <summary>
+/// Defines a contract for calculating technical analysis indicators that operate on multiple series of stock data points.
+/// </summary>
+/// <remarks>
+/// Implementations of this interface are designed to process one or more series of stock data points and produce
+/// indicator results that can be used for financial analysis. Each series represents a collection of stock data
+/// (e.g., open, high, low, close, volume) over a period of time.
+/// </remarks>
+public interface IMultiSeriesIndicator
+{
+    IReadOnlyList<IndicatorResult> Calculate(IReadOnlyList<IReadOnlyList<StockDataPoint>> series);
+    
+}
